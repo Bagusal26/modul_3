2893e8231cff2f4ddc9871fac43167f412818984
bagusal26 <bagusazhar01@gmail.com>
2024-10-31 23:33:46 +0700

    jefj

--- src/Main.java
+++ src/Main.java
@@ -133,6 +133,7 @@ class AplikasiKontak {
             System.out.println("2. Hapus Kontak");
             System.out.println("3. Daftar Kontak");
             System.out.println("4. Keluar");
+            System.out.println("5. Keluar");
             String pilihan = scanner.nextLine();
 
             switch (pilihan) {
